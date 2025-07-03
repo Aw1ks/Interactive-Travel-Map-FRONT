1cb0094f05082071a6dd4a24ccf3b3a888f69e06
die fals <145365323+Aw1ks@users.noreply.github.com>
2025-07-03 22:31:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,72 +14,35 @@ function openCustomPopup(content) {
     popup_container.className = 'popup-container';
 
 
-/* ------SLIDER------ */
-    const slider = document.createElement('slider');
-    slider.className = 'icon-cards mt-3';
-
-    const contentDiv = document.createElement('div');
-    contentDiv.className = 'icon-cards__content';
-
-    const images = [
-        'images/media/Russian_landmark_1.jpg',
-        'images/media/Russian_landmark_2.jpg',
-        'images/media/Russian_landmark_3.jpg'
-    ];
-
-    images.forEach(src => {
-        const img = document.createElement('img');
-        img.src = src;
-        img.className = 'icon-cards__item d-flex align-items-center justify-content-center'
-        contentDiv.appendChild(img);
-    });
-
-    slider.appendChild(contentDiv);
-
-    const checkboxDiv = document.createElement('div');
-    checkboxDiv.className = 'checkbox';
-
-    const checkbox = document.createElement('input');
-    checkbox.className = 'd-none';
-    checkbox.id = 'toggle-animation';
-    checkbox.type = 'checkbox';
-    checkbox.checked = true;
-
-    checkboxDiv.appendChild(checkbox);
-    document.body.appendChild(checkboxDiv);
-
-    function classToggle() {
-        var el = document.querySelector('.icon-cards__content');
-        el.classList.toggle('step-animation');
-    }
-
-    document.querySelector('#toggle-animation').addEventListener('click', classToggle);
-
-/* ------ТЕКСТОВАЯ ИНФОРМАЦИЯ------ */
+    
+    /* ------ТЕКСТОВАЯ ИНФОРМАЦИЯ------ */
+    const background_heading = document.createElement('div');
+    background_heading.className = 'background-heading'
+    
     const heading = document.createElement('h2');
     heading.className = 'country-name';
     heading.textContent = content.country;
-
-    const countryText = document.createElement('h3');
-    countryText.className = 'city-name';
-    countryText.textContent = `Текущий город - ${content.city}`;
-
+    
     const country_information_container = document.createElement('div');
     country_information_container.className = 'country-information_container';
-
-    const country_information = document.createElement('div');
-    country_information.className = 'country-information';
-    country_information.textContent = 'Информация о стране';
-
+    
     const country_description = document.createElement('div');
     country_description.className = 'country-description';
     country_description.textContent = 'Российская Федерация — крупнейшее в мире государство, занимающее 1/8 часть суши и расположенное на северо-востоке Евразии. Россия — страна с многовековой историей, богатым культурным наследием и щедрой природой. В России можно найти почти всё то, что встречает путешественник по отдельности в той или иной стране — солнечные пляжи субтропиков и снежные горные вершины, бескрайние степи и глухие леса, бурные реки и тёплые моря.' 
-
+    
+    /* ------FLAG------ */
+    const country_flag_container = document.createElement('div');
+    country_flag_container.className = 'country-flag-container';
+    
+    const country_flag = document.createElement('img');
+    country_flag.className = 'country-flag';
+    country_flag.src = 'images/media/flags/test_flag.png';
+    
+    /* ------COMMENTS------ */
     const traveler_reviews = document.createElement('div');
     traveler_reviews.className = 'traveler-reviews';
-    traveler_reviews.textContent = 'Отзывы путешественников:';
+    traveler_reviews.textContent = 'Впечатления путешественников';
 
-/* ------КОММЕНТАРИИ------ */
     const commentsContainer = document.createElement('div');
     commentsContainer.className = 'comments-popup_container';
 
@@ -87,17 +50,34 @@ function openCustomPopup(content) {
         {
             avatar: 'images/icons/avatar.png',
             message: 'Первый комментарий!',
-            likes: 10
         },
         {
             avatar: 'images/icons/avatar.png',
             message: 'Второй комментарий.',
-            likes: 5
+        },
+        {
+            avatar: 'images/icons/avatar.png',
+            message: 'Второй комментарий.',
+        },
+        {
+            avatar: 'images/icons/avatar.png',
+            message: 'Второй комментарий.',
+        },
+        {
+            avatar: 'images/icons/avatar.png',
+            message: 'Второй комментарий.',
+        },
+        {
+            avatar: 'images/icons/avatar.png',
+            message: 'Второй комментарий.',
+        },
+        {
+            avatar: 'images/icons/avatar.png',
+            message: 'Второй комментарий.',
         },
         {
             avatar: 'images/icons/avatar.png',
             message: 'Третий комментарий, который скрыт по умолчанию.',
-            likes: 2
         }
     ];
 
@@ -113,18 +93,8 @@ function openCustomPopup(content) {
         message.className = 'message';
         message.textContent = comment.message;
 
-        const like_image = document.createElement('img');
-        like_image.className = 'like_image';
-        like_image.src = 'images/icons/like_icon.png';
-
-        const likes = document.createElement('div');
-        likes.className = 'likes';
-        likes.textContent = `${comment.likes}`;
-
         commentDiv.appendChild(avatar);
         commentDiv.appendChild(message);
-        commentDiv.appendChild(likes);
-        commentDiv.appendChild(like_image);
 
         commentsContainer.appendChild(commentDiv);
     });
@@ -148,13 +118,8 @@ function openCustomPopup(content) {
                 message.className = 'message';
                 message.textContent = comment.message;
 
-                const likes = document.createElement('div');
-                likes.className = 'likes';
-                likes.textContent = `${comment.likes}`;
-
                 commentDiv.appendChild(avatar);
                 commentDiv.appendChild(message);
-                commentDiv.appendChild(likes);
 
                 commentsContainer.appendChild(commentDiv);
             });
@@ -164,15 +129,56 @@ function openCustomPopup(content) {
         };
     }
 
-/* ------ДОБАВЛЯЕМ ЭЛЕМЕНТЫ НА СТРАНИЦУ------ */
-    popup_container.appendChild(slider);
-    popup_container.appendChild(heading);
-    popup_container.appendChild(countryText);
+    /* ------КОММЕНАРИЙ ПОЛЬЗОВАТЕЛЯ------ */
+    const user_comment_container = document.createElement('div');
+    user_comment_container.className = 'user-comment_container';
+
+    const user_comment = document.createElement('input');
+    user_comment.className = 'user-comment';
+    user_comment.type = 'text'; 
+    user_comment.placeholder = 'Введите комментарий'; 
+
+    const upload_image_container = document.createElement('div');
+    upload_image_container.className = 'upload_image_container';
+    
+    const upload_image = document.createElement('input');
+    upload_image.className = 'upload-image';
+    upload_image.type = 'file'; 
+    upload_image.accept = 'images/media'; 
+    upload_image.multiple = false; 
+
+    const send_comment = document.createElement('button');
+    send_comment.className = 'send-comment';
+    send_comment.textContent = 'Отправить'; 
+
+    const uploadButton = document.createElement('button');
+    uploadButton.className = 'upload-image';
+    uploadButton.innerText = 'Выбрать изображение';
+
+    uploadButton.onclick = () => {
+        upload_image.click();
+    };
+
+    
+    
+    user_comment_container.appendChild(user_comment);
+    user_comment_container.appendChild(upload_image_container);
+    upload_image_container.appendChild(upload_image);
+    upload_image_container.appendChild(uploadButton);
+    upload_image_container.appendChild(upload_image);
+    user_comment_container.appendChild(send_comment);
+
+    /* ------ДОБАВЛЯЕМ ЭЛЕМЕНТЫ НА СТРАНИЦУ------ */
+    popup_container.appendChild(background_heading);
+    background_heading.appendChild(heading)
     popup_container.appendChild(country_information_container);
-    country_information_container.appendChild(country_information);
     country_information_container.appendChild(country_description);
+    country_information_container.appendChild(country_flag_container);
+    country_flag_container.appendChild(country_flag);
     popup_container.appendChild(traveler_reviews);
     popup_container.appendChild(commentsContainer);
+    popup_container.appendChild(user_comment_container);
+
     if (expandButton) commentsContainer.appendChild(expandButton);
 
     popupDiv.appendChild(popup_container);
@@ -180,15 +186,15 @@ function openCustomPopup(content) {
     document.body.appendChild(popupDiv);
 
     setTimeout(() => {
-        popupDiv.classList.add('show');
+            popupDiv.classList.add('show');
 
-        const currentCenter_open = map.getCenter();
-        map.panTo([currentCenter_open.lat, currentCenter_open.lng + 0.2], { animate: true });
+            const currentCenter_open = map.getCenter();
+            map.panTo([currentCenter_open.lat, currentCenter_open.lng + 0.2], { animate: true });
     }, 10);
 
     activePopup = popupDiv;
 
-/* ------ЗАКРЫТИЕ ОКНА------- */
+    /* ------ЗАКРЫТИЕ ОКНА------- */
     let close_window = document.createElement('div');
     close_window.className = 'close-window';
 
